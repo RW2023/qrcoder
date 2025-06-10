3847f2f256a2428e9275279f12b675af8281fcd8
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2025-06-10 09:32:03 -0400

    refactor: add initial Tailwind CSS configuration file

---
+++ tailwind.config.ts
@@ -0,0 +1,15 @@
+import { type Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./app/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      //  add fonts or colors here if needed
+    },
+  },
+};
+
+export default config;
